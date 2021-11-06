1e8513dc66e0acc9673e1302ca5b3acb8636c955
adhammhaydat <adhammohidat123@gmail.com>
2021-11-06 14:53:50 +0200

    to ashrf

---
+++ tests/test_bord.py
@@ -0,0 +1,12 @@
+from tic.game_logic import Game_logic
+from flo import diff
+def test_quitter():
+    game=Game_logic()
+    diffs = diff(game.play, path="to_test.sim.txt")
+    assert not diffs, diffs
+# def test_bord():
+    
+#     values = [' ' for x in range(9)]
+#     a=print_tic_tac_toe(values)
+
+#     assert expected==a
